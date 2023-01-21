3759983f653599948e509f035068593e916aac93
jingug1004 <jingug1004@gmail.com>
2023-01-21 20:02:17 +0900

    feat: part 7 Builder - 복잡한 인스턴스를 조합한다
    이것도 별로 안 쓰일 듯. 실용적이지 않음. abstract 보다 implements를 쓰는 걸 더 선호할 듯 해

---
+++ src/A06Builder/HtmlMainBuilder.java
@@ -0,0 +1,34 @@
+package A06Builder;
+
+public class HtmlMainBuilder {
+
+    public static void main(String[] args) {
+//        if (args.length != 1) {
+//            usage();
+//            System.exit(0);
+//        }
+
+        if (false) {
+            TextBuilder textBuilder = new TextBuilder();
+            Director director = new Director(textBuilder);
+            director.construct();
+            String result = textBuilder.getTextResult();
+            System.out.println("l~ result : " + result);
+        } else if (true) {
+            HTMLBuilder htmlBuilder = new HTMLBuilder();
+            Director director = new Director(htmlBuilder);
+            director.construct();
+            String filename = htmlBuilder.getHTMLResult();
+            System.out.println("l~ HTML 파일 : " + filename + "이 작성되었습니다.");
+        } else {
+            usage();
+            System.exit(0);
+        }
+
+    }
+
+    public static void usage() {
+        System.out.println("Usage: java Main text 텍스트 문서 작성");
+        System.out.println("Usage: java Main html HTML 파일로 문서 작성");
+    }
+}
