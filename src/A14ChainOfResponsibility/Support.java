4fec0c0dc94facdd7571335a5836f3372fc17c8b
jingug1004 <jingug1004@gmail.com>
2023-02-04 21:16:30 +0900

    feat: part 14 Chain of Responsibility - 책임을 떠넘긴다
    어렵네. 이거 실제로 쓰는 로직이나 서비스가 있을까? 완전 무쓸모 같은데

---
+++ src/A14ChainOfResponsibility/Support.java
@@ -0,0 +1,41 @@
+package A14ChainOfResponsibility;
+
+public abstract class Support {
+    private String name;
+    private Support next;
+
+    public Support(String name) {
+        this.name = name;
+        this.next = null;
+    }
+
+    public Support setNext(Support next) {
+        this.next = next;
+        return next;
+    }
+
+    public void support(Trouble trouble) {
+        if (resolve(trouble)) {
+            done(trouble);
+        } else if (next != null) {
+            next.support(trouble);
+        } else {
+            fail(trouble);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "[" + name + "]";
+    }
+
+    protected abstract boolean resolve(Trouble trouble);
+
+    protected void done(Trouble trouble) {
+        System.out.println("l~ " + trouble + " is resolved by " + this + ".");
+    }
+
+    protected void fail(Trouble trouble) {
+        System.out.println("l~ " + trouble + " cannot be resolved.");
+    }
+}
---
+++ src/A14ChainOfResponsibility/Trouble.java
@@ -0,0 +1,18 @@
+package A14ChainOfResponsibility;
+
+public class Trouble {
+    private int number;
+
+    public Trouble(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return "[Trouble " + number + "]";
+    }
+}
