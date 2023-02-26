8d1eea3d7c1e04de2d49ab72f757559330174391
jingug1004 <jingug1004@gmail.com>
2023-02-26 19:28:49 +0900

    feat: part 18 Memento - 상태 변화를 저장한다
    안됨. ㅠ_ㅠ

---
+++ src/A18Memento/DiceMainMemento.java
@@ -0,0 +1,33 @@
+package A18Memento;
+
+public class DiceMainMemento {
+
+    public static void main(String[] args) {
+        Gamer gamer = new Gamer(100);
+        Memento memento = gamer.createMemento();
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println("==== " + i);
+            System.out.println("상태:" + gamer);
+
+            gamer.bet();
+
+            System.out.println("소지금은 " + gamer.getMoney() + "원이 되었습니다.");
+
+            if (gamer.getMoney() > memento.getMoney()) {
+                System.out.println("X많이 늘었으니 현재 상태를 저장하자!");
+                memento = gamer.createMemento();
+            } else if (gamer.getMoney() < memento.getMoney() / 2) {
+                System.out.println("X많이 줄었으니 이전 상태를 복원하자!");
+                gamer.restoreMemento(memento);
+            }
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+            System.out.println();
+        }
+    }
+}
