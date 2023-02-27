2edc018bb7ccf50a67ccb65afeab8fe51d53f527
jingug1004 <jingug1004@gmail.com>
2023-02-27 20:30:38 +0900

    feat: part 19 State - 상태 클래스로 표현한다
    별로

---
+++ src/A19State/SecuMainState.java
@@ -0,0 +1,20 @@
+package A19State;
+
+public class SecuMainState {
+
+    public static void main(String[] args) {
+        SafeFrame frame = new SafeFrame("State Sample");
+
+        while (true) {
+            for (int hour = 0; hour < 24; hour++) {
+                frame.setClock(hour);
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+
+                }
+            }
+        }
+    }
+}
